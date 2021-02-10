ab4c880547d20f3563b1d0f34a2c878d8dd07cfa
Karl <37496339+aquarial@users.noreply.github.com>
2021-02-10 17:30:38 -0600

    just manage ~zod, not ~wet yet

--- data/restore-backups.sh
+++ data/restore-backups.sh
@@ -8,20 +8,20 @@ fi
 
 if [ -f "./data/fakezod.tar" ]; then
     echo "restore piers from backups"; sleep 2;
-    rm -rf "./data/fakezod" "./data/fakewet"
+    rm -rf "./data/fakezod" # "./data/fakewet"
     tar xf "./data/fakezod.tar" --strip-components 1
-    tar xf "./data/fakewet.tar" --strip-components 1
+    # tar xf "./data/fakewet.tar" --strip-components 1
 else
 
     # create piers if needed
     if [ ! -d "./data/fakezod" ]; then
         echo "create piers"; sleep 2;
         "./data/urbit" -x -F zod -c "./data/fakezod"
-        "./data/urbit" -x -F wet -c "./data/fakewet"
+        # "./data/urbit" -x -F wet -c "./data/fakewet"
     fi
 
     echo "create backups"; sleep 2;
     tar cf "./data/fakezod.tar" "./data/fakezod"
-    tar cf "./data/fakewet.tar" "./data/fakewet"
+    # tar cf "./data/fakewet.tar" "./data/fakewet"
 fi
 
