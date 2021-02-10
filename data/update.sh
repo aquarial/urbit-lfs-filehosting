8122a47b7e8bb942b8309a016c7e638189fb6906
Karl <37496339+aquarial@users.noreply.github.com>
2021-02-10 17:02:45 -0600

    script to manage piers

--- data/update.sh
+++ data/update.sh
@@ -1,21 +1,27 @@
-data=$(dirname "$(readlink -f "$0")")
+if [ ! -f "./data/urbit" ]; then
+    echo "File not found: ./data/urbit"
+    echo "wrong directory?"
+    exit 0;
+fi
 
-if [ -f "$data/fakezod.tar"]; then
-    # restore piers from backups
-    rm -rf "$data/fakezod" "$data/fakewet"
-    tar xf "$data/fakezod.tar"
-    tar xf "$data/fakewet.tar"
+# Restore piers from backups, or create backups
+
+if [ -f "./data/fakezod.tar" ]; then
+    echo "restore piers from backups"; sleep 2;
+    rm -rf "./data/fakezod" "./data/fakewet"
+    tar xf "./data/fakezod.tar"
+    tar xf "./data/fakewet.tar"
 else
 
     # create piers if needed
-    if [ ! -d "$data/fakezod"]; then
-        # create piers
-        "$data/urbit" -x -F zod -c "$data/fakezod"
-        "$data/urbit" -x -F wet -c "$data/fakewet"
+    if [ ! -d "./data/fakezod" ]; then
+        echo "create piers"; sleep 2;
+        "./data/urbit" -x -F zod -c "./data/fakezod"
+        "./data/urbit" -x -F wet -c "./data/fakewet"
     fi
 
-    # backups
-    tar cf "$data/fakezod.tar" "$data/fakezod"
-    tar cf "$data/fakewet.tar" "$data/fakewet"
+    echo "create backups"; sleep 2;
+    tar cf "./data/fakezod.tar" "./data/fakezod"
+    tar cf "./data/fakewet.tar" "./data/fakewet"
 fi
 
