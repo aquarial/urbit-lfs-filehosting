5a92217d8dad0865595253a4421d9fd0f4514f7e
Karl <37496339+aquarial@users.noreply.github.com>
2021-02-11 15:04:47 -0600

    use ~molnut comet to test

--- data/restore-backups.sh
+++ data/restore-backups.sh
@@ -6,22 +6,22 @@ fi
 
 # Restore piers from backups, or create backups
 
-if [ -f "./data/fakezod.tar" ]; then
+if [ -f "./data/molnut.tar" ]; then
     echo "restore piers from backups"; sleep 2;
-    rm -rf "./data/fakezod" # "./data/fakewet"
-    tar xf "./data/fakezod.tar" --strip-components 1
+    rm -rf "./data/molnut" # "./data/fakewet"
+    tar xf "./data/molnut.tar" --strip-components 1
     # tar xf "./data/fakewet.tar" --strip-components 1
 else
 
     # create piers if needed
-    if [ ! -d "./data/fakezod" ]; then
+    if [ ! -d "./data/molnut" ]; then
         echo "create piers"; sleep 2;
-        "./data/urbit" -x -F zod -c "./data/fakezod"
+        "./data/urbit" -x -F zod -c "./data/molnut"
         # "./data/urbit" -x -F wet -c "./data/fakewet"
     fi
 
     echo "create backups"; sleep 2;
-    tar cf "./data/fakezod.tar" "./data/fakezod"
+    tar cf "./data/molnut.tar" "./data/molnut"
     # tar cf "./data/fakewet.tar" "./data/fakewet"
 fi
 
