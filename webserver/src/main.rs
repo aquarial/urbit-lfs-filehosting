254b0791da31ac0d71fd756a0183722f5bd0bedf
Karl <37496339+aquarial@users.noreply.github.com>
2021-03-05 17:48:25 -0600

    Add authenticated header

--- webserver/src/main.rs
+++ webserver/src/main.rs
@@ -2,18 +2,45 @@
 
 #[macro_use] extern crate rocket;
 
-use std::io::Read;
 use std::fs::File;
 use std::collections::HashMap;
 use std::sync::RwLock;
 
-use rocket::State;
-use rocket::Data;
+use lazy_static::lazy_static;
+
+use rocket::{State, Data};
+use rocket::http::Status;
 use rocket::response::Stream;
+use rocket::request::{FromRequest, Request, Outcome};
+
+
+lazy_static! {
+    static ref AUTH_KEY: RwLock<String> = RwLock::new(String::new());
+}
+
+struct AuthToken {}
+
+#[derive(Debug)]
+enum AuthTokenError { InvalidToken }
+
+impl<'a, 'r> FromRequest<'a, 'r> for AuthToken {
+    type Error = AuthTokenError;
+
+    fn from_request(request: &'a Request<'r>) -> Outcome<Self, Self::Error> {
+        let token = request.headers().get_one("auth_token");
+        if let Some(token) = token {
+            let key = AUTH_KEY.read().unwrap();
+            if key.bytes().zip(token.bytes()).filter(|(a,b)| a != b).count() == 0 {
+                return Outcome::Success(AuthToken {})
+            }
+        }
+        return Outcome::Failure((Status::Unauthorized, AuthTokenError::InvalidToken));
+    }
+}
 
 struct Info {
     upload_paths: RwLock<HashMap<String, ()>>,
-    download_paths: RwLock<HashMap<String, ()>>
+    download_paths: RwLock<HashMap<String, ()>>,
 }
 
 impl Info {
@@ -59,8 +86,12 @@ fn download_file(key: String) -> Stream<File> {
     Stream::from(f)
 }
 
-
 fn main() {
+    {
+        let mut key = AUTH_KEY.write().unwrap();
+        *key = String::from("hunter2");
+        println!("Authorized Header is {}", *key);
+    }
     std::fs::create_dir_all("./files/").unwrap();
     rocket::ignite()
         .manage(Info::new())
