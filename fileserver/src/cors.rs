c29cbce88f41c8d88b1082035cbeacd5debbf204
Karl <37496339+aquarial@users.noreply.github.com>
2021-04-13 13:49:39 -0500

    remove cors debugging prints

--- fileserver/src/cors.rs
+++ fileserver/src/cors.rs
@@ -18,13 +18,10 @@ impl Fairing for CORS {
 
     fn on_response(&self, request: &Request, response: &mut Response) {
         if request.method() == Method::Options || request.uri().path().starts_with("/upload/file/") {
-            println!("allowing cors");
             response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
             response.set_header(Header::new("Access-Control-Allow-Methods", "OPTIONS, POST"));
             response.set_header(Header::new("Access-Control-Allow-Headers", "Content-Type"));
             response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
-        } else {
-            println!("NOT allowing cors");
         }
 
         if request.method() == Method::Options {
