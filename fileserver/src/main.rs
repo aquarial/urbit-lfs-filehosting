b0e7ec1d9678f0cb8871a05e7f3615caa8b32497
Karl <37496339+aquarial@users.noreply.github.com>
2021-07-15 20:25:29 -0500

    get minimal 0.5.0 working

--- fileserver/src/main.rs
+++ fileserver/src/main.rs
@@ -7,34 +7,30 @@ use std::fs::File;
 use std::collections::{HashMap, HashSet};
 use std::sync::{Mutex, RwLock};
 
-use lazy_static::lazy_static;
-
 use rocket::{State, Data};
 use rocket::http::Status;
-use rocket::response::{Response, NamedFile};
+use rocket::response::{Response};
 use rocket::response::status::NotFound;
 use rocket::request::{FromRequest, Request, Outcome};
 use rocket::data::ToByteUnit;
 
 use reqwest::blocking::{Client};
 
-
-lazy_static! {
-    static ref AUTH_KEY: RwLock<String> = RwLock::new(String::new());
-}
+static mut AUTH_KEY: String = String::new();
 
 struct AuthToken {}
 
 #[derive(Debug)]
 enum AuthTokenError { InvalidToken }
 
-impl<'a, 'r> FromRequest<'a, 'r> for AuthToken {
+#[rocket::async_trait]
+impl<'r> FromRequest<'r> for AuthToken {
     type Error = AuthTokenError;
 
-    fn from_request(request: &'a Request<'r>) -> Outcome<Self, Self::Error> {
+    async fn from_request(request: &'r Request<'_>) -> Outcome<Self, Self::Error> {
         let token = request.headers().get_one("authtoken");
         if let Some(token) = token {
-            let key = AUTH_KEY.read().unwrap();
+            let key = unsafe { &AUTH_KEY };
             // compare each byte to prevent timing attack
             if key.bytes().zip(token.bytes()).filter(|(a,b)| a != b).count() == 0 {
                 return Outcome::Success(AuthToken {})
@@ -68,100 +64,104 @@ fn default() -> &'static str {
     "fileserver online\n"
 }
 
-
-#[post("/setup", data = "<data>")]
-fn setup_provider(_tok: AuthToken, state: State<Info>, data: Data) -> &'static str {
-    let mut provider = String::new();
-    data.open(1000.bytes()).read_to_string(&mut provider).unwrap();
-    let mut url = state.provider_url.lock().unwrap();
-    *url = Some(provider);
-    let mut ups = state.upload_paths.write().unwrap();
-    ups.clear();
-    "setup provider\n"
+#[get("/secure")]
+fn default_secure(tok: AuthToken) -> &'static str {
+    "fileserver secure\n"
 }
 
-
-#[post("/upload/new/<key>/<space>")]
-fn upload_new(_tok: AuthToken, state: State<Info>, key: String, space: u64) -> &'static str {
-    let mut ups = state.upload_paths.write().unwrap();
-    println!("available for uploading {} bytes with {}", space, key);
-    ups.insert(key, space);
-    "upload path active\n"
-}
-
-
-#[options("/upload/file/<_key>")]
-fn options_handler<'a>(_key: String) -> &'static str {
-    ""
-}
-
-#[post("/upload/file/<key>", data = "<data>")]
-fn upload_file(state: State<Info>, key: String, data: Data) -> &'static str {
-    if key.contains("..") {
-        return "invalid key\n";
-    }
-    let mut ups = state.upload_paths.write().unwrap();
-    match ups.remove(&key) {
-        Some(size) => {
-            // TODO handle file-system errors
-            let mut f = File::create(format!("./files/{}", key)).unwrap();
-            let written = data.open(size.bytes()).stream_precise_to(&mut f).unwrap();
-            f.sync_all().unwrap();
-
-            let url = state.provider_url.lock().unwrap();
-            let auth: &str = &*AUTH_KEY.read().unwrap();
-            let url2: String = format!("{}/~lfs/completed/{}/{}/padding", (*url).as_ref().unwrap(), key, hoon_format_num(written));
-            println!("curling to {}", url2);
-            let res = state.client
-                .post(url2)
-                .header("authtoken", auth)
-                .send();
-
-            match res {
-                Ok(res) => {
-                    println!("Got resposne: {:?}", res);
-                    if res.status() == 200 {
-                        println!("uploaded file {}", key);
-                        std::mem::drop(ups);
-                        return "uploaded\n";
-                    } else {
-                        println!("Error uploading {}", key);
-                        ups.insert(key, size);
-                    }
-                },
-                Err(err) => {
-                    println!("Error uploading {}: {:?}", key, err);
-                    ups.insert(key, size);
-                }
-            }
-            return "could not confirm upload with provider. try again when it's online\n"
-        }
-        None => {
-            println!("no path to upload {}", key);
-            return "no such path\n";
-        }
-    }
-}
-
-fn hoon_format_num(n: u64) -> String {
-    if n >= 1000 {
-        let mut s = hoon_format_num(n / 1000);
-        s.push_str(&format!(".{:0>3}", n % 1000));
-        s
-    } else {
-        format!("{}", n)
-    }
-}
-
-
-#[get("/download/file/<key>")]
-fn download_file(key: String) -> Result<NamedFile, NotFound<String>> {
-    // TODO: any other security concerns?
-    if key.contains("..") || key.contains("/") {
-        return Err(NotFound("invalid path".into()));
-    }
-    NamedFile::open(&format!("./files/{}", key)).map_err(|e| NotFound(e.to_string()))
-}
+// #[post("/setup", data = "<data>")]
+// fn setup_provider(_tok: AuthToken, state: State<Info>, data: Data) -> &'static str {
+//     let mut provider = String::new();
+//     data.open(1000.bytes()).read_to_string(&mut provider).unwrap();
+//     let mut url = state.provider_url.lock().unwrap();
+//     *url = Some(provider);
+//     let mut ups = state.upload_paths.write().unwrap();
+//     ups.clear();
+//     "setup provider\n"
+// }
+// 
+// 
+// #[post("/upload/new/<key>/<space>")]
+// fn upload_new(_tok: AuthToken, state: State<Info>, key: String, space: u64) -> &'static str {
+//     let mut ups = state.upload_paths.write().unwrap();
+//     println!("available for uploading {} bytes with {}", space, key);
+//     ups.insert(key, space);
+//     "upload path active\n"
+// }
+// 
+// 
+// #[options("/upload/file/<_key>")]
+// fn options_handler<'a>(_key: String) -> &'static str {
+//     ""
+// }
+// 
+// #[post("/upload/file/<key>", data = "<data>")]
+// fn upload_file(state: State<Info>, key: String, data: Data) -> &'static str {
+//     if key.contains("..") {
+//         return "invalid key\n";
+//     }
+//     let mut ups = state.upload_paths.write().unwrap();
+//     match ups.remove(&key) {
+//         Some(size) => {
+//             // TODO handle file-system errors
+//             let mut f = File::create(format!("./files/{}", key)).unwrap();
+//             let written = data.open(size.bytes()).stream_precise_to(&mut f).unwrap();
+//             f.sync_all().unwrap();
+// 
+//             let url = state.provider_url.lock().unwrap();
+//             let auth: &str = &*AUTH_KEY.read().unwrap();
+//             let url2: String = format!("{}/~lfs/completed/{}/{}/padding", (*url).as_ref().unwrap(), key, hoon_format_num(written));
+//             println!("curling to {}", url2);
+//             let res = state.client
+//                 .post(url2)
+//                 .header("authtoken", auth)
+//                 .send();
+// 
+//             match res {
+//                 Ok(res) => {
+//                     println!("Got resposne: {:?}", res);
+//                     if res.status() == 200 {
+//                         println!("uploaded file {}", key);
+//                         std::mem::drop(ups);
+//                         return "uploaded\n";
+//                     } else {
+//                         println!("Error uploading {}", key);
+//                         ups.insert(key, size);
+//                     }
+//                 },
+//                 Err(err) => {
+//                     println!("Error uploading {}: {:?}", key, err);
+//                     ups.insert(key, size);
+//                 }
+//             }
+//             return "could not confirm upload with provider. try again when it's online\n"
+//         }
+//         None => {
+//             println!("no path to upload {}", key);
+//             return "no such path\n";
+//         }
+//     }
+// }
+// 
+// fn hoon_format_num(n: u64) -> String {
+//     if n >= 1000 {
+//         let mut s = hoon_format_num(n / 1000);
+//         s.push_str(&format!(".{:0>3}", n % 1000));
+//         s
+//     } else {
+//         format!("{}", n)
+//     }
+// }
+// 
+// 
+// #[get("/download/file/<key>")]
+// fn download_file(key: String) -> Result<NamedFile, NotFound<String>> {
+//     // TODO: any other security concerns?
+//     if key.contains("..") || key.contains("/") {
+//         return Err(NotFound("invalid path".into()));
+//     }
+//     NamedFile::open(&format!("./files/{}", key)).map_err(|e| NotFound(e.to_string()))
+// }
 
 
 /// https://rust-lang-nursery.github.io/rust-cookbook/algorithms/randomness.html
@@ -181,34 +181,38 @@ fn generate_password(len: usize) -> String {
     password
 }
 
-#[delete("/upload/remove/<key>")]
-fn upload_remove(_tok: AuthToken, state: State<Info>, key: String) -> &'static str {
-    if key.contains("..") {
-        return "invalid key\n";
-    }
-    let mut ups = state.upload_paths.write().unwrap();
-    println!("removing upload path to {}", key);
-    ups.remove(&key);
-    std::fs::remove_file(format!("./files/{}", key)).unwrap();
-    "upload path removed\n"
-}
+//#[delete("/upload/remove/<key>")]
+//fn upload_remove(_tok: AuthToken, state: State<Info>, key: String) -> &'static str {
+//    if key.contains("..") {
+//        return "invalid key\n";
+//    }
+//    let mut ups = state.upload_paths.write().unwrap();
+//    println!("removing upload path to {}", key);
+//    ups.remove(&key);
+//    std::fs::remove_file(format!("./files/{}", key)).unwrap();
+//    "upload path removed\n"
+//}
 
-fn main() {
+#[launch]
+fn rocket() -> _ {
     let args: HashSet<String> = std::env::args().map(|s| s.to_ascii_lowercase()).collect();
 
     {
-        let mut key = AUTH_KEY.write().unwrap();
-        if args.contains("--unsafe_debug_auth") {
-            *key = "hunter2".into()
+        let key: String = if args.contains("--unsafe_debug_auth") {
+            "hunter2".into()
         } else {
-            *key = generate_password(60);
+            generate_password(60)
+        };
+        println!("Authorized Header is {}", key);
+        unsafe {
+            AUTH_KEY = key;
         }
-        println!("Authorized Header is {}", *key);
     }
+    // args.contains("--add-cors-headers")
 
     std::fs::create_dir_all("./files/").unwrap();
     rocket::build()
-        .manage(Info::new())
-        .attach(cors::CORS { enabled: args.contains("--add-cors-headers")})
-        .mount("/", routes![default, upload_new, upload_file, upload_remove, download_file, setup_provider, options_handler]);
+        //.manage(Info::new())
+        .mount("/", routes![default, default_secure])
+    //  .mount("/", routes![default, upload_new, upload_file, upload_remove, download_file, setup_provider, options_handler])
 }
